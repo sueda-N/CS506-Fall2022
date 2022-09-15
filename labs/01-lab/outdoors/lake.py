613597397718d0a97b69e31f4279085e2c5102f4
Ayse Nalcaci <snalcaci@bu.edu>
2022-09-15 13:06:17 -0400

    add outdoors part of the city

--- labs/01-lab/outdoors/lake.py
+++ labs/01-lab/outdoors/lake.py
@@ -1,3 +1,14 @@
+
 def draw_lake():
-    print("lake not found")
-    return
+    row = 12
+    col = 12
+    print("This is a lake.")
+    for i in range(0,row):
+        for j in range(0,col):
+            if((j == 0 or j == col-1) and (i!=0 and i!=row-1)) :
+                print('*',end='')   
+            elif( ((i==0 or i==row-1) and (j>0 and j<col-1))):
+                print('*',end='')
+            else:
+                print(end=' ') 
+        print()  
--- labs/01-lab/outdoors/park.py
+++ labs/01-lab/outdoors/park.py
@@ -1,3 +1,15 @@
 def draw_park():
-    print("park not found")
+    print("This is a park")
+    
+    
+    side = 9
+    i = 0
+
+    while(i < side):
+        j = 0
+        while(j < side):      
+            j = j + 1
+            print('*', end = '  ')
+        i = i + 1
+        print('')
     return
